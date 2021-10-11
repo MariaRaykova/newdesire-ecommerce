717b897ad9515e0691cccacf31d6822d4b57a62c
unknown <mimi.raykova@gmail.com>
2021-10-11 11:32:41 +0300

    initial commit

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const dbConnection = require('./config/database');
 const express = require('express');
 const app = require('express')();
 const path = require('path');
+const bodyParser = require('body-parser');
 app.use(function(req, res, next) {
     res.setHeader("Content-Type", "application/json");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     next();
 });
 //     prevents cors headaches when your react app calls your api
 // serves the built version of your react app
 app.use(express.static(path.join(__dirname, '/client/build')))
+console.log(__dirname, '/client/build')
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/json
+// app.use(bodyParser.json());
+
 dbConnection().then(() => {
   
     require('./config/express')(app);
@@ -26,7 +34,7 @@ dbConnection().then(() => {
         res.status(500).send(err.message);
         console.log('*'.repeat(90))
     });
-
+   
     app.listen(config.port, console.log(`Listening on port ${config.port}!`))
 
 }).catch(console.error);
