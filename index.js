8df2d367aa5721d7f3e494d2813433227c3fc28e
unknown <mimi.raykova@gmail.com>
2021-10-11 16:42:20 +0300

    initial commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
      res.setHeader('Access-Control-Expose-Headers', 'Content-Type, Authorization,Origin,X-Requested-With,Accept');
      // Request methods you wish to allow
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-     res.setHeader('Content-Encoding', 'gzip');
+    //  res.setHeader('Content-Encoding', 'gzip');
      // Request headers you wish to allow
     //  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
  
