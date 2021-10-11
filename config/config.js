9c7d82b4195d80747ce08374515e64741270b39e
unknown <mimi.raykova@gmail.com>
2021-10-11 19:09:58 +0300

    initial commit

--- config/config.js
+++ config/config.js
@@ -8,6 +8,7 @@ const config = {
         STRIPE_PUBLISHABLE: "pk_test_51JdAdCF4Yqr45yd9fJBb7uulrO4Q53z6yUmxWbmKZwij2gbxlIzXZpZCdHzp6wD2jOyzfiO67n5BEKDbKCYwcF3Z00LU9IjWkv",
         STRIPE_SECRET_KEY: "sk_test_51JdAdCF4Yqr45yd9i965sqbZiaWiLtmwsWbtBYsk8JRWiCT99nQHZ6qQYawgOFIazchREfbGo4iVvOZt91dAyK4M00tz0gsAF0",
     },
+    //mongodb+srv://mraykova:maria123@cluster0.n9z04.mongodb.net/eshop?retryWrites=true&w=majority
     production: {
         port: process.env.PORT || 9999,
         dbURL:'mongodb+srv://mraykova:maria123@cluster0.qlvbe.mongodb.net/eshop?retryWrites=true&w=majority',
